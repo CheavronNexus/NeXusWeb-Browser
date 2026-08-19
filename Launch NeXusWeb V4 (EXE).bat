fff1e407a3f25159ce5de89b7cd93c987cdc77d3
Bhaskar Patwardhan <mr.ray_mond@outlook.com>
2026-08-19 13:19:43 +0530

    Restrut

--- Launch NeXusWeb V4 (EXE).bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title NeXusWeb V4 (Next-Gen Developer & Privacy Browser)
-cd /d "%~dp0prototype V4"
-echo Starting NeXusWeb V4...
-start "" "%~dp0prototype V4\dist-electron\NeXusWeb-V4-win32-x64\NeXusWeb-V4.exe"
-exit
